5d6317bf82829b8974d4b42d763f501e3d12a9a6
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-06-27 22:33:17 +0500

    Week 1 Day 1 Using lambda for multiply two numbers

--- Week 1/Day1.py
+++ Week 1/Day1.py
@@ -1,7 +1,8 @@
 # In First week we cover the Python basics 
 
-#  1 (Functions)
-#Create a simple function which add two numbers and return result.
+#  Topic no 01  (Functions)
+
+# Create a simple function which add two numbers and return result.
 
 def add(a,b):
     return a+b
@@ -9,3 +10,17 @@ def add(a,b):
 result = add(5,10)
 
 print(result)
+
+# Create a lambda function which multiply two numbers
+
+Create a function using lambda which multiply two numbers 
+
+multiply = lambda num1, num2: num1 * num2
+num1 = int(input("Enter number 1 : "))
+num2 = int(input("Enter number 2 : "))
+
+result = multiply(num1, num2)
+
+print(result)
+
+
