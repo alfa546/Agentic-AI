cdaa46cecebe4afae8d891801499403215af1294
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-06-27 22:16:28 +0500

    Week 1 Day 1 Function syantax

--- Week 1/Day1.py
+++ Week 1/Day1.py
@@ -1,1 +1,11 @@
 # In First week we cover the Python basics 
+
+#  1 (Functions)
+#Create a simple function which add two numbers and return result.
+
+def add(a,b):
+    return a+b
+
+result = add(5,10)
+
+print(result)
