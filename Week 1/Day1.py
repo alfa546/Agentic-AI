34fd87f3e221f0467a75170fdb44bb8941add143
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-06-27 22:48:31 +0500

    Week 1 Day 1 Find factorial of a number

--- Week 1/Day1.py
+++ Week 1/Day1.py
@@ -21,16 +21,32 @@
 
 # print(result)
 
-# Global variable 
+# Global variable  (global variable is variable which is delared from outside the function)
 
-global_var = " I am Nouman sajid "
+# global_var = " I am Nouman sajid "
 
-def Functin1 ():
- print("Hellow" , global_var)
+# def Functin1 ():
+#  print("Hellow" , global_var)
 
-def Funation2 ():
-   print("hi", global_var)
+# def Funation2 ():
+#    print("hi", global_var)
 
-Functin1()
-Funation2()
+# Functin1()
+# Funation2()
 
+# Exercise (Solve problems using funactions)
+
+# Problem no 1 :
+# Write a recursive function to calculate the factorial of a non-negative integer.
+# Exercise Purpose: Factorials (5! = 5 * 4 * 3 * 2 * 1) are the textbook example of recursion. This exercise reinforces the concept of a “Base Case” (stopping at 1) to prevent the function from running forever and crashing the memory stack.
+# Given Input: number = 5
+# Expected Output: The factorial of 5 is 120
+
+def recursive_factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * recursive_factorial(n - 1)
+
+num = 5
+print ("The factorial of num is ", recursive_factorial(num))
