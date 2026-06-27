2005ff05c3b839e98d73b19e33569645097ccc6e
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-06-27 22:38:10 +0500

    Week 1 Day 1 Global variable

--- Week 1/Day1.py
+++ Week 1/Day1.py
@@ -4,23 +4,33 @@
 
 # Create a simple function which add two numbers and return result.
 
-def add(a,b):
-    return a+b
+# def add(a,b):
+#     return a+b
 
-result = add(5,10)
+# result = add(5,10)
 
-print(result)
+# print(result)
 
-# Create a lambda function which multiply two numbers
+# # Create a lambda function which multiply two numbers 
 
-Create a function using lambda which multiply two numbers 
+# multiply = lambda num1, num2: num1 * num2
+# num1 = int(input("Enter number 1 : "))
+# num2 = int(input("Enter number 2 : "))
 
-multiply = lambda num1, num2: num1 * num2
-num1 = int(input("Enter number 1 : "))
-num2 = int(input("Enter number 2 : "))
+# result = multiply(num1, num2)
 
-result = multiply(num1, num2)
+# print(result)
 
-print(result)
+# Global variable 
 
+global_var = " I am Nouman sajid "
+
+def Functin1 ():
+ print("Hellow" , global_var)
+
+def Funation2 ():
+   print("hi", global_var)
+
+Functin1()
+Funation2()
 
