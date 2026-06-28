738d58c639816f2505eb2c52f689c530b8d9185f
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-06-28 20:31:14 +0500

    Week 1 Day 2 (OOPS and ASYNC programming) : Inheritance

--- Week 1/Day2(OOPS,Asyn programming).py
+++ Week 1/Day2(OOPS,Asyn programming).py
@@ -1,11 +1,36 @@
 # Class
 
-class Person:
-    def __init__(self, name, age):
-       self.name = name
-       self.age =age
+# class Person:
+#     def __init__(self, name, age):
+#        self.name = name
+#        self.age =age
 
-person1 =Person("Nouman",32)
+# person1 =Person("Nouman",32)
 
-print(person1.name)
-print(person1.age)
+# print(person1.name)
+# print(person1.age)
+
+# Inheritance 
+
+class specie :
+    def __init__(self,name,age):
+     self.name = name
+     self.age = age
+
+    def barks(self):
+     print("Dog barks")
+
+class Dog(specie):
+    def __init__(self,name,age,breed):
+       self.name=name
+       self.age=age
+       self.breed=breed
+
+    def night(self):
+       print("Dog barks at night")
+
+D = specie("Shaepard",2)
+D = Dog("Lebra",3,"Ousto")
+
+D.barks()
+D.night()
