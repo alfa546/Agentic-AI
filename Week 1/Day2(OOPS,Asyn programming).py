846a8b0e549f04440ed12e0dd751fd2e1af1f70b
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-06-28 20:15:08 +0500

    Week 1 Day 2 (OOPS and ASYNC programming) : Class initalize

---
+++ Week 1/Day2(OOPS,Asyn programming).py
@@ -0,0 +1,11 @@
+# Class
+
+class Person:
+    def __init__(self, name, age):
+       self.name = name
+       self.age =age
+
+person1 =Person("Nouman",32)
+
+print(person1.name)
+print(person1.age)
